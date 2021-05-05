26515a6e41632b8c223c9bd2fd7256782f9a661f
Eric Thomas <edt514@gmail.com>
2021-05-04 23:36:59 -0400

    Implement design

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+   
+?>
+<!doctype html>
+<html lang="en">
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="author" content="Eric Thomas">
+   <meta name="description" content="Eric Thomas recreation of the DevChallenges 404 page challenge">
+   <meta name="keywords" content="404, error, devchallenges">
+   <title> 404 Not Found </title>
+    
+   <link rel="shortcut icon" href="img/icon.png">    
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/404.css">
+   <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Inconsolata">
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@700&display=swap" rel="stylesheet">
+   <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <header class="row">
+            <div class="col-sm-12 col-md-2">
+              <br>
+              <h3 class="uppercase">
+                  404 Not Found
+              </h3>
+           </div>
+           <div class="col-md-10"> &nbsp; </div>
+        </header>
+        <div class="clear"> &nbsp; </div>
+        <section class="row">
+            <aside class="col-sm-12 col-md-6 hero center-text">
+                <img src="img/scarecrow.png" title="Something's gone wrong" alt="404 scarecrow">
+            </aside>
+            <div class="col-sm-12 col-md-6">
+                <div class="clear"> &nbsp; </div>
+                <h1 class="title">
+                    I have bad news for you
+                </h1>
+                <br>
+                <p class="message">
+                    The page you are looking for might be removed or is temporarily unavailable
+                </p>
+                <br>
+                <a href="#" class="btn btn-dark btn-back">
+                    Back to Homepage
+                </a>
+            </div>
+        </section>
+        <div class="clear"> &nbsp; </div>
+        <div class="clear"> &nbsp; </div>
+        <footer class="row">
+            <div class="col-sm-12 center-text">
+                created by <a href="https://github.com/ETstudios" target="_blank">FaeWilds</a> - <a href="https://devchallenges.io/" target="_blank">devChallenges.io</a>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
